13068e845ccd44efe73b6373b3a77d991666f9a4
lorran2003 <lorran23082003@gmail.com>
2024-03-19 12:28:48 -0300

    retirando facebook e atualizando layout

--- src/js/principal.jsx
+++ src/js/principal.jsx
@@ -4,28 +4,32 @@ export function Main() {
             <nav className="navBar">
                 <ul>
                     <li id=" linha1">
-                        <div>
-                            <i id="logo1" className="fa-brands fa-instagram"><a href="https://www.instagram.com/felicitafestasrio?igsh=dm5mYW4ydXpodzE4"></a></i>
+                        <div className="link1">
+                            <a href="https://www.instagram.com/felicitafestasrio?igsh=dm5mYW4ydXpodzE4">
+                                <i className="fa-brands fa-instagram">
+                                </i>
+                            </a>
+
                         </div>
-                        <a href="https://www.instagram.com/felicitafestasrio?igsh=dm5mYW4ydXpodzE4" target="_blank">Instagram</a>
+                        <a className="link link1" href="https://www.instagram.com/felicitafestasrio?igsh=dm5mYW4ydXpodzE4" target="_blank">Instagram</a>
                     </li>
                     <li id=" linha2">
-                        <div>
-                            <i id="logo2" className="fa-brands fa-facebook-messenger"><a href="https://m.me/angela.delima.165"></a></i>
+                        <div className="link2">
+                            <a href="https://m.me/angela.delima.165">
+                                <i className="fa-brands fa-facebook-messenger">
+                                </i>
+                            </a>
                         </div>
-                        <a href="https://m.me/angela.delima.165" target="_blank">Messenger</a>
-                    </li>
-                    <li id=" linha3">
-                        <div>
-                            <i id="logo3" className="fa-brands fa-facebook"><a href="https://www.facebook.com/angela.delima.165/"></a></i>
-                        </div>
-                        <a href="https://www.facebook.com/angela.delima.165/" target="_blank">Facebook</a>
+                        <a className="link link2" href="https://m.me/angela.delima.165" target="_blank">Messenger</a>
                     </li>
                     <li id=" linha4">
                         <div id="divWPP">
-                            <i id="logo4" className="fa-brands fa-whatsapp"><a href="https://wa.me/5521981315814"></a></i>
+                            <a href="https://wa.me/5521981315814">
+                                <i className="fa-brands fa-whatsapp">
+                                </i>
+                            </a>
                         </div>
-                        <a href="https://wa.me/5521981315814" target="_blank">Whatsapp</a>
+                        <a className="link link3" href="https://wa.me/5521981315814" target="_blank">Whatsapp</a>
                     </li>
                 </ul>
             </nav>
